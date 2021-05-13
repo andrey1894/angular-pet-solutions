5391c0cbb52a0f5d12880fb26c91eaba76c76f44
Andrey Lebedev <lebedev.andrey1894@gmail.com>
2021-05-13 10:24:32 +0300

    Feat: Add material theme. Common app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HeaderComponent, AsideComponent } from './core/components';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideComponent,
+    HeaderComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule,
